bf1febed9ade1ae024dcbcfdc2c928d05c2b0e49
Ubaid <ubaidwazir2003@gmail.com>
2026-02-11 17:32:24 +0500

    FIX: default_auto_config typos fixed in accounts and management apps

--- src/services/management/apps.py
+++ src/services/management/apps.py
@@ -5,7 +5,7 @@ class ManagementConfig(AppConfig):
     name = 'src.services.management'
     verbose_name = 'Management'
     verbose_plural = 'Management'
-    default_auto_config = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         import src.core.signals # noqa: F401
